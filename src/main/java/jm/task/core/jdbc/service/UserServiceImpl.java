405f55cac31f6cd19fb0dda8097ca0cca643e97d
sebeb-siz <sebebsiz93@gmail.com>
2023-05-25 20:15:04 +0300

    initial changes

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private final UserDao uDao = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
         uDao.createUsersTable();
     }
